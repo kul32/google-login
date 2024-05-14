f8307be3979900ffb6630af1d0dc1814d115b1d4
KULSUM SHAIK <115711134+kul32@users.noreply.github.com>
2024-05-14 17:26:49 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,8 @@
+function login() {
+    var email = document.getElementById("email").value;
+    var password = document.getElementById("password").value;
+    if (email !== "" && password !== "") {
+        // Display login success message
+        alert("Login successful!");
+    } 
+}
